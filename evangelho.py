3dedc1f3c3658a1ff3c9514da3e69b68e249b836
brendolalves <brendol.alves@gmail.com>
2026-04-14 15:47:33 -0300

    Atualização do evangelho.py
    Foi criada a instancia evangelho para melhorar a identificação e alteração dos códigos (encapsulamento), no futuro será criado um app.py para o envio dos dados retirados.

--- evangelho.py
+++ evangelho.py
@@ -1,33 +1,33 @@
 import requests
-from bs4 import BeautifulSoup, Comment
+from bs4 import BeautifulSoup
 
-def buscar_evangelho():
-    url = "https://www.paulus.com.br/portal/liturgia-diaria/"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/110.0.0.0'}
 
-    try:
-        res = requests.get(url, headers=headers)
-        res.encoding = 'utf-8'
-        soup = BeautifulSoup(res.text, 'html.parser')
+class Evangelho:
+    def __init__(self):
+        self.url = "https://www.paulus.com.br/portal/liturgia-diaria/"
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/110.0.0.0'}
 
-        conteudo_final = ""
+    def obter_dados(self):
+        try:
+            res = requests.get(self.url, headers=self.headers)
+            res.encoding = 'utf-8'
+            soup = BeautifulSoup(res.text, 'html.parser')
+            conteudo_final = ""
+            texto_bruto = soup.get_text(separator="\n")
+            
+            if "Evangelho:" in texto_bruto:
+                conteudo_final = texto_bruto.split("Evangelho:")[1].split("Palavra da salvação.")[0].split()
 
-        #comentarios = soup.find_all(string=lambda text: isinstance(text, Comment))
+                return conteudo_final if conteudo_final else "Não foi possivel localizar o Evangelho"
+        except Exception as e:
+            return f"Erro na conexão: {e}"
 
-        #for coment in comentarios:
-        texto_bruto = soup.get_text(separator="\n")
-        if "Evangelho:" in texto_bruto:
-            conteudo_final = texto_bruto.split("Evangelho:")[1].split("Palavra da salvação.")[0].split()
-
-        return conteudo_final if conteudo_final else "Não foi possivel localizar o Evangelho"
-    except Exception as e:
-        return f"Erro na conexão: {e}"
-    
-resultado = buscar_evangelho()
+evangelho = Evangelho()
+resultado = evangelho.obter_dados()
 
 with open('Evangelho_do_dia.txt', "w", encoding="utf-8") as f:
     f.write("Evangelho do Dia\n\n")
-    f.write(' '.join(resultado))
+    f.write(' '.join(resultado) if isinstance(resultado, list) else resultado)
     f.write("\n")
     f.write("Palavra da Salvação\n\n")
 
